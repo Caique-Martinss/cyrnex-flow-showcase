7f352387bbbf2567aba72edaab143f02359ccdd9
Caique Martins <164359305+Caique-Martinss@users.noreply.github.com>
2026-09-04 11:42:45 -0300

    Remove incorrectly uploaded source files

--- AppPages.tsx
+++
@@ -1,8 +0,0 @@
-import { api } from './http';
-
-export async function sendSimulatedMessage(
-  clientId: string,
-  message: string
-): Promise<void> {
-  await api.post('/messages/send', { clientId, message });
-}
--- waitlist.api.ts
+++
@@ -1,7 +0,0 @@
-import type { BusinessSubscription } from '../domain/subscription.types';
-import { api } from './http';
-
-export async function getSubscriptionStatus(): Promise<BusinessSubscription> {
-  const response = await api.get<BusinessSubscription>('/subscription/status');
-  return response.data;
-}
